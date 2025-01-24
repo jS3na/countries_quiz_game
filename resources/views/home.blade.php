d94758ac84e0c22700769367622b2c0d9316040d
js3na <passosjoaogabriel29@gmail.com>
2025-01-24 18:12:20 -0300

    feat: home components

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-main-layout pageTitle="Home">
+
+</x-main-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,4 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home');
